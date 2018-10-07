384e514df68968a1e2c69b51d061cf2bbf8ed67f
GuillaumeNury <gnury@lucca.fr>
2018-10-08 00:56:28 +0200

    [fix] missing jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
-  collectCoverage: true
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: true,
+  moduleNameMapper: {
+    '@core/models': '<rootDir>/src/core/models',
+    '@core/services': '<rootDir>/src/core/services',
+  },
 };
